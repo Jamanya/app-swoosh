42ad6d92778bd53be8a39c457c92c3bf07af3f90
Jamanya <richardson.james@outlook.com>
2017-08-02 15:48:51 -0400

    added data model

--- Swoosh/Swoosh/Controller/LeagueVC.swift
+++ Swoosh/Swoosh/Controller/LeagueVC.swift
@@ -10,20 +10,35 @@ import UIKit
 
 class LeagueVC: UIViewController {
     
+    var player: Player!
+    
+    
+    @IBOutlet weak var nextBTN: BorderButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        player = Player()
+    }
+
     
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "SkillVCSegue", sender: self)
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBAction func onMensTapped(_ sender: Any) {
+       selectLeague(leagueType: "mens")
     }
-
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "wommens")
+    }
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
     
-
     
+    func selectLeague(leagueType: String){
+        player.desiredLeague = leagueType
+        nextBTN.isEnabled = true
+    }
     
 
 }
---
+++ Swoosh/Swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  Swoosh
+//
+//  Created by James Richardson on 8/2/17.
+//  Copyright © 2017 Jamanya Apps. All rights reserved.
+//
+
+import Foundation
+
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
